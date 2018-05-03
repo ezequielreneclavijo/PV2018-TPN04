c9ea27d80ab761ad2c609ca0d2f6f91b956ef001
EZEQUIEL <EZEQUIEL@EZEQUIEL-PC>
2018-05-02 23:04:49 -0300

    Se agregó un paquete controlador beans forms y  una clase form bean

---
+++ src/java/pto1/aplicacion/controlador/beans/forms/TemperaturaFormBean.java
@@ -0,0 +1,21 @@
+
+package pto1.aplicacion.controlador.beans.forms;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author EZEQUIEL
+ */
+@ManagedBean
+@ViewScoped
+public class TemperaturaFormBean {
+
+    /**
+     * Creates a new instance of TemperaturaFormBean
+     */
+    public TemperaturaFormBean() {
+    }
+    
+}
